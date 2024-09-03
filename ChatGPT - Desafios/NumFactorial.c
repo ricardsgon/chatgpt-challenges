5373137a9619bb1bd5e277bdad5b44a80bffe3dc
Ricardo Gonçalves <162488386+ricardsgon@users.noreply.github.com>
2024-09-03 16:03:19 -0400

    Can now hold more values

--- ChatGPT - Desafios/NumFactorial.c
+++ ChatGPT - Desafios/NumFactorial.c
@@ -1,21 +1,20 @@
 /*Crie um programa em C que calcule o fatorial de um número inteiro positivo fornecido pelo usuário.*/
 #include <stdio.h>
 #include <locale.h>
-int calc(int x);
+unsigned long long calc(int x);
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
     int numero;
     printf("Insira seu número: ");
     scanf("%d", &numero);
-    int calculo = calc(numero);
-    printf("Seu fatorial vai ser: %d", calculo);
+    printf("Seu fatorial vai ser: %llu", calc(numero));
 }
 
-int calc(int x)
+unsigned long long calc(int x)
 {
-    int i, calcu = x;
-    for(i = 1; i < x; i++){
+    unsigned long long calcu = x;
+    for(int i = 1; i < x; i++){
         calcu = calcu * i;
     }
     return calcu;
