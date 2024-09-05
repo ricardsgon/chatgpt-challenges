4bccde9de5a848677d9b88547e7c05f33ed23829
Ricardo Gonçalves <162488386+ricardsgon@users.noreply.github.com>
2024-09-05 13:01:00 -0400

    Update

--- ChatGPT - Desafios/ListaAlunos.c
+++ ChatGPT - Desafios/ListaAlunos.c
@@ -1,68 +1,39 @@
 /*Crie um programa em C para gerenciar as notas de alunos em uma turma.*/
+
 #include <stdio.h>
 #include <locale.h>
 #include <string.h>
 
-typedef struct
-{
-    char nome[20];
-    int nota[4];
-    int media;
-} aluno;
+#define ESTUDANTE_MAX 500
 
-int main()
-{
+struct estudante{
+    char nome[50];
+    int média;
+};
+
+void addEstudante(struct estudante student[], int *contador);
+void showEstudante(struct estudante student[], int *contador);
+
+int main(){
     setlocale(LC_ALL, "Portuguese");
-    int menu, i, quan;
-    aluno struc[50];
-    aluno *pointer;
-    int numaluno = 0;
-    int max = sizeof(struc) / sizeof(struc[0]);
 
-    do
-    {
-        printf("SIMULADOR DE SISTEMA ESTUDANTIL\n1 - Adicionar Aluno\n2 - Listar Alunos\n3 - Sair\n");
-        printf("--> ");
-        scanf("%d", &menu);
-        getchar();
-        switch (menu)
-        {
-        case 1:
-            printf("Insira a quantidade de alunos(Max - %d): ", max);
-            scanf("%d", &quan);
-            getchar();
-            if (quan <= max)
-            {
-                for (i = 0; i < quan; i++)
-                {
-                    pointer = &struc[i];
-                    printf("Nome do Aluno %d: ", i + 1);
-                    fgets(pointer->nome, sizeof(pointer->nome), stdin);
-                    pointer->nome[strcspn(pointer->nome, "\n")]=0;
-                    printf("  Média do Aluno: ");
-                    scanf("%d", &pointer->media);
-                    getchar();
-                }
-                numaluno += quan;
-            }
-            printf("\n");
-            break;
-        case 2:
+}
 
-        case 3:
-            if (numaluno == 0)
-            {
-                printf("Nenhum Aluno no Sistema.\n");
-                break;
-            }
-            for (i = 0; i < numaluno; i++)
-            {
-                pointer = &struc[i];
-                printf("Nome %d: %s\n", i + 1, pointer->nome);
-                printf("  Média: %d\n", pointer->media);
-                printf("\n");
-            }
-        }
-    } while (menu != 3);
-    return 0;
+void addEstudante(struct estudante student[], int *contador){
+    if(*contador <= ESTUDANTE_MAX){
+        printf("Digite o nome do aluno: ");
+        scanf("%s", &student[*contador].nome);
+        printf("Digite a média do aluno: ");
+        scanf("%d", &student[*contador].média);
+        printf("\n");
+        printf("Aluno adicionado com sucesso.\n");
+        *contador++;
+    }else{
+        printf("Limite de alunos atingido.");
+    }
+}
+
+void showEstudante(struct estudante student[], int *contador){
+    for(int i = 0;)
+    printf("%s", s)
 }
