ee1daab73725edaae85bad56a2734d5ec4823680
Ricardo Gonçalves <162488386+ricardsgon@users.noreply.github.com>
2024-09-05 21:45:11 -0400

    Added more info

--- ChatGPT - Desafios/ListaAlunos.c
+++ ChatGPT - Desafios/ListaAlunos.c
@@ -6,34 +6,56 @@
 
 #define ESTUDANTE_MAX 500
 
-struct estudante{
+typedef struct war
+{
     char nome[50];
-    int média;
-};
+    int media;
+}estudante;
 
-void addEstudante(struct estudante student[], int *contador);
-void showEstudante(struct estudante student[], int *contador);
+void addEstudante(estudante student[], int *contador);
+void showEstudante(estudante student[], int *contador);
 
-int main(){
+int main()
+{
     setlocale(LC_ALL, "Portuguese");
-
+    int menu, contador;
+    estudante Student[ESTUDANTE_MAX];
+    int *Pointer = &Student[ESTUDANTE_MAX]; 
+    do{
+        printf("SIMULADOR DE SISTEMA ESTUDANTIL\n1 - Adicionar Aluno\n2 - Listar alunos\n3 - Sair\n");
+        printf("--> ");
+        scanf("%d", &menu);
+        switch(menu){
+            case 1: 
+                addEstudante(*Pointer, contador);
+                break;
+        }
+    }while (menu != 3);
 }
 
-void addEstudante(struct estudante student[], int *contador){
-    if(*contador <= ESTUDANTE_MAX){
+void addEstudante(estudante student[], int *contador)
+{
+    if (*contador <= ESTUDANTE_MAX)
+    {
         printf("Digite o nome do aluno: ");
         scanf("%s", &student[*contador].nome);
         printf("Digite a média do aluno: ");
-        scanf("%d", &student[*contador].média);
+        scanf("%d", &student[*contador].media);
         printf("\n");
         printf("Aluno adicionado com sucesso.\n");
         *contador++;
-    }else{
+    }
+    else
+    {
         printf("Limite de alunos atingido.");
     }
 }
 
-void showEstudante(struct estudante student[], int *contador){
-    for(int i = 0;)
-    printf("%s", s)
+void showEstudante(estudante student[], int *contador)
+{
+    for (int i = 0; i < *contador; i++)
+    {
+        printf("Nome do aluno %d: %s\n", i + 1, student[*contador].nome);
+        printf("Média do aluno %d: %s\n", i + 1, student[*contador].media);
+    }
 }
