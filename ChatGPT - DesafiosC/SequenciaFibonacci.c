51d2a93a5a14479349d40d2f31b45f652e9477cd
Ricardo Gonçalves <162488386+ricardsgon@users.noreply.github.com>
2024-10-02 12:01:09 -0400

    Update SequenciaFibonacci.c

--- ChatGPT - DesafiosC/SequenciaFibonacci.c
+++ ChatGPT - DesafiosC/SequenciaFibonacci.c
@@ -1,22 +1,37 @@
 /*Implementar um programa em C que receba um número n e imprima os primeiros n números da sequência de Fibonacci.*/
 #include <stdio.h>
 #include <locale.h>
+#include <stdlib.h>
 
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
-    int num, arr[100];
+    int num;
+    long *arr = NULL;
+    printf("Insira a quantidade de números da sequência de Fibonnaci que deseja ver: ");
+    scanf("%d", &num);
+    arr = (long *)malloc(sizeof(long) * num);
     arr[0] = 0;
     arr[1] = 1;
-    printf("Insira um número: ");
-    scanf("%d", &num);
-    for (int i = 2; i <= num; i++)
+    if (num == 1)
     {
-        arr[i] = arr[i - 1] + arr[i - 2];
+        printf("%ld", arr[0]);
     }
-    for (int i = 0; i <= num; i++)
+    else if (num == 2)
     {
-        printf("%d ", arr[i]);
+
+        printf("%ld %ld", arr[0], arr[1]);
+    }
+    else
+    {
+        for (int i = 2; i < num; i++)
+        {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        for (int i = 0; i < num; i++)
+        {
+            printf("%ld ", arr[i]);
+        }
     }
     printf("\n");
     return 0;
